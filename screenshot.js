fe2b6cb57c789fc9c2d3e762ce97c03601992346
Rudransh upreti <126793635+rudranshupreti@users.noreply.github.com>
2024-10-26 15:12:24 +0530

    first commit

---
+++ screenshot.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'] // Essential flags for Render
+  });
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com');
+
+  // Take a screenshot and save it as "screenshot.png"
+  await page.screenshot({ path: 'screenshot.png' });
+  console.log("Screenshot taken and saved as screenshot.png");
+
+  await browser.close();
+})();
